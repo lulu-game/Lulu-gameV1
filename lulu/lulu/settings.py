8df838835c52be386d7b86de1f91d75aa166ac03
david Njoroge <davidndegwa6869@gmail.com>
2024-03-10 11:31:47 +0300

    Dev: BUild

--- lulu/lulu/settings.py
+++ lulu/lulu/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lulu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.jnbvgadsisgbanexlxig',
+        'PASSWORD': 'Qwertyis_6869.',
+        'HOST': 'aws-0-eu-west-2.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 
---
+++ luluv1/lulu_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LuluApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lulu_api'
